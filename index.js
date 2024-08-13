319a44cf3a9ed4183e21e82c48177685a161d91b
jangiddeepak <royalxtic@gmail.com>
2024-08-13 15:47:26 +0530

    page 4

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-import LocomotiveScroll from 'locomotive-scroll';
 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#ain'),
+    el: document.querySelector('#main'),
     smooth: true
 });
+var elems = document.querySelector("#elem-container");
+
+var fixedI = document.querySelector("#fixed-image");
+elems.addEventListener("mouseenter", function () {
+    fixedI.style.display = "block"
+})
+elems.addEventListener("mouseleave", function () {
+    fixedI.style.display = "none"
+});
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var image = e.getAttribute("data-image")
+        fixedI.style.backgroundImage = `url(${image})`
+    })
+});
+let loader = document.getElementById("loader")
+setTimeout(function(){
+    loader.style.top = "-100%"
+},4000)
+
+
+
+
